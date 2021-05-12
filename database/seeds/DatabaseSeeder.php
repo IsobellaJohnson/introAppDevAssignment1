ceba8fd5f50c262af19356d3c1983c63463e3319
IsobellaJohnson <64507442+IsobellaJohnson@users.noreply.github.com>
2021-05-12 12:48:59 +1200

    chaanged seeder order

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,10 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class); 
+        //$this->call(UserSeeder::class); 
         $this->call(RatingSeeder::class);
         $this->call(ReviewerSeeder::class);
-       $this ->call(MovieSeeder::class);
+        $this ->call(MovieSeeder::class);
 
     }
 }
